9c1fa6f393e29fe8bfc3acd1ca7a976dd6901ffa
Ronnie Ghose <1313566+RONNCC@users.noreply.github.com>
2018-09-17 23:17:16 -0700

    add db backups

--- email-script.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$to = 'jyoung96@berkeley.edu';
-    $email = $_POST['event_post_comment'];
-    $subject = 'Test';
-    $message = 'Will this actually work?';
-
-    mail($to, $email, $subject, $message);
-?>
--- logout.php
+++
@@ -1,4 +0,0 @@
-<?php
-include 'include/includes.php';
-$g_user->logout();
-?>
